ed1d6e489981f7f9dd3302a38f6a3a32dd86ea34
Christian <cjzamora0296@gmail.com>
2020-03-19 10:19:56 +0800

    facility model and admin

--- facility_reservation/settings.py
+++ facility_reservation/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'facility.apps.FacilityConfig',
     'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
