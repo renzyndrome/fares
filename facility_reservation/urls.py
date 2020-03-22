1cd5dab713685a970e195c16b04a13363fbaa4e5
Christian <cjzamora0296@gmail.com>
2020-03-22 15:13:37 +0800

    view users

--- facility_reservation/urls.py
+++ facility_reservation/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
+    path('view_user/<int:user_id>/', user_views.view_user, name='view_user'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('password-reset/',
@@ -23,6 +24,7 @@ urlpatterns = [
     path('password-reset-complete/',
                     auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
                     name='password_reset_complete'),
+    path('user_list/', user_views.user_list, name='user_list')
 ]
 
 if settings.DEBUG:
